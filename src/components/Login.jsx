c2ea34758951e0f374def543509f6e670050567a
Rituraj Pandey <68937122+TheWeKi@users.noreply.github.com>
2023-01-31 20:54:16 +0530

    Initial setup

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+function Login() {
+    return (
+        <>
+            <h1 className="text-bg-danger">
+                This is A Login Web App 2.0
+            </h1>
+        </>
+    );
+}
+
+export {Login};
