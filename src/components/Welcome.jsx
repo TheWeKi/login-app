9ba24270381d3fae4a3e940bce5c15845d124ae1
Rituraj Pandey <rp.weki.24@gmail.com>
2023-02-06 17:38:12 +0530

    welcome page completed

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,7 +2,21 @@
 export function Welcome() {
     return(
         <>
-            <h1>Hello Welcome</h1>
+                <section className="vh-100" style={{background: "linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1))"}}>
+                    <div className="container py-5 h-100">
+                        <div className="row d-flex justify-content-center align-items-center h-100">
+                            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                                <div className="card bg-dark text-white" style={{borderRadius: "1rem"}}>
+                                    <div className="card-body p-5 text-center">
+                                            <h2 className="fw-bold text-uppercase">
+                                                Welcome To This Page
+                                            </h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
         </>
     )
 }
