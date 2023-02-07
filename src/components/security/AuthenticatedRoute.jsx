d76a8fead329a457541c3c53b6860c897ca5299c
Rituraj Pandey <rp.weki.24@gmail.com>
2023-02-07 20:09:10 +0530

    Authenticated Route done

--- src/components/security/AuthenticatedRoute.jsx
+++ src/components/security/AuthenticatedRoute.jsx
@@ -5,7 +5,6 @@ function AuthenticatedRoute( { children } ) {
 
     const useAuth = useAuthContext();
 
-    console.log(useAuth.isAuthenticated)
     if(useAuth.isAuthenticated) return children
     return <Navigate to='/' />
 }
